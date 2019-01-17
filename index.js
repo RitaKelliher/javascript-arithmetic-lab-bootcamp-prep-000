fb965d045f6fe47bf2197839b072f22bdba94737
Rita Musello-Kelliher <ritamkell@gmail.com>
2019-01-17 19:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,11 @@ function dec(n) {
   return n--
 }
 function makeInt(n) {
+ console.log(parseInt(n, 10))
  return parseInt(n, 10)
 }
+function preserveDecimal(n) {
+  console.log(parseFloat(n,10))
+  return parseFloat(n, 10)
+}
+
